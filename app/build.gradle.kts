ffb9fa7d5136296c71b44c31682d1c76e146868a
Paulo Sabaini <pasabaini@gmail.com>
2022-10-11 09:10:34 -0300

    migrate gradle to KTS and add version catalog

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.junit)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktLint)
+}
+
+android {
+    compileSdk = 33
+    namespace = "org.sabaini.thrones"
+
+    defaultConfig {
+        applicationId = "org.sabaini.thrones"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "org.sabaini.thrones.HiltTestRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+        )
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+    packagingOptions {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+
+    implementation(libs.bundles.common)
+    implementation(libs.material)
+    implementation(libs.roomKtx)
+    testImplementation(libs.bundles.commonTest)
+    androidTestImplementation(libs.testAndroidCompose)
+    androidTestImplementation(libs.testAndroidCore)
+    androidTestImplementation(libs.testAndroidHilt)
+    androidTestImplementation(libs.testAndroidRunner)
+
+    kapt(libs.hiltCompiler)
+    ksp(libs.roomCompiler)
+    kaptAndroidTest(libs.testAndroidHiltCompiler)
+
+    coreLibraryDesugaring(libs.desugar)
+
+    detektPlugins(libs.detektTwitterCompose)
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
--- app/src/main/java/org/sabaini/thrones/MainActivity.kt
+++ app/src/main/java/org/sabaini/thrones/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import org.sabaini.thrones.ui.theme.ThronesTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,12 +31,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ThronesTheme {
-        Greeting("Android")
-    }
 }
