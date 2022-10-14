2abcb7e2cefe627e495c32819dbd1cad59696c51
Paulo Sabaini <pasabaini@gmail.com>
2022-10-14 08:58:46 -0300

    add network stuff di

---
+++ core/src/main/java/org/sabaini/thrones/core/network/NetworkModule.kt
@@ -0,0 +1,72 @@
+package org.sabaini.thrones.core.network
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sabaini.thrones.core.BuildConfig
+import retrofit2.Retrofit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val INTERCEPTOR_LOGGING_NAME = "INTERCEPTOR_LOGGING"
+
+    @Provides
+    @Named(INTERCEPTOR_LOGGING_NAME)
+    fun provideHttpLoggingInterceptor(): Interceptor {
+        return if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        } else {
+            noOpInterceptor()
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        @Named(INTERCEPTOR_LOGGING_NAME) loggingInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .apply {
+                addNetworkInterceptor(loggingInterceptor)
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+        val contentType = "application/json".toMediaType()
+
+        return Retrofit
+            .Builder()
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .baseUrl(BuildConfig.THRONES_API_URL)
+            .client(okHttpClient)
+            .build()
+    }
+
+    private fun noOpInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            chain.proceed(chain.request())
+        }
+    }
+}
