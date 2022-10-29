d1626e6395a1e084a638e46a16125956117f6775
Paulo Sabaini <pasabaini@gmail.com>
2022-10-29 09:52:16 -0300

    removed wildcard import

--- core/src/main/java/org/sabaini/thrones/core/base/BaseViewModel.kt
+++ core/src/main/java/org/sabaini/thrones/core/base/BaseViewModel.kt
@@ -5,7 +5,12 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
